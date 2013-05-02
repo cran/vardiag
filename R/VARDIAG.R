6add33ae7dcaccbcb98e7406dc58b48c05a62786
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2013-05-02 00:00:00 +0000

    version 0.2-0

--- R/VARDIAG.R
+++ R/VARDIAG.R
@@ -1,4 +1,15 @@
-p11 <<- p22 <<- p12 <<- p21 <<- par()
+#assign("p11", NULL, envir = .GlobalEnv)
+#assign("p12", NULL, envir = .GlobalEnv)
+#assign("p21", NULL, envir = .GlobalEnv)
+#assign("p22", NULL, envir = .GlobalEnv)
+
+#p11 <<- par() p22 <<- par() p12 <<- par() p21 <<- par()
+#p11 <- par() p22 <- par() p12 <- par() p21 <- par()
+.varDiagOptions <- new.env(FALSE, globalenv())
+assign("p11", NULL, envir = .varDiagOptions)
+assign("p12", NULL, envir = .varDiagOptions)
+assign("p21", NULL, envir = .varDiagOptions)
+assign("p22", NULL, envir = .varDiagOptions)
 
 gamsph<-function(h,th=rbind(1,1,1)){(0<h)*(h<=th[3])*(th[1]+th[2]*(3/2*(h/th[3])-1/2*(h/th[3])^3))+(h>th[3])*(th[1]+th[2])}
 
@@ -174,73 +185,128 @@ print.varobj<-function(x,...){print(x$pars); print(x$lof);invisible(x)}
 #r[row(r)<col(r)]<-v$res
 #r<-r+t(r)
 
-PlotDiag.varobj<-function(v,region=0,xyi=0,zmv=0){
+PlotDiag.varobj<-function(v, region = NULL, xyi = 0, zmv = 0) {
 
-palette(c("black","cyan","magenta","green3","yellow","blue","white","red"))
+  palette(c("black","cyan","magenta","green3","yellow","blue","white","red"))
 
-n<-length(v$h)
-infm<-solve(v$cg);
-cv<-chol((infm+t(infm))/2);
-XM<-cbind(gamsph1(v$h,v$pars),gamsph2(v$h,v$pars),gamsph3(v$h,v$pars))*(gamsph(v$h,v$pars))^(-0.75)/4
-Vare<-v$cg-XM%*%solve(t(XM)%*%solve(v$cg,XM),t(XM))
-#sig<-mean(sqrt(diag(Vare)))
-e<-v$res
-sig<-sqrt(sum(e^2)/(n-3))
-gdd<-(2^0.25*gamma(0.75)/sqrt(pi))*gamsph(v$h,v$pars)^0.25+e*sig/sqrt(diag(Vare))
+  n<-length(v$h)
+  infm<-solve(v$cg);
+  cv<-chol((infm+t(infm))/2);
+  XM<-cbind(gamsph1(v$h,v$pars),gamsph2(v$h,v$pars),gamsph3(v$h,v$pars))*(gamsph(v$h,v$pars))^(-0.75)/4
+  Vare<-v$cg-XM%*%solve(t(XM)%*%solve(v$cg,XM),t(XM))
+  #sig<-mean(sqrt(diag(Vare)))
+  e<-v$res
+  sig<-sqrt(sum(e^2)/(n-3))
+  gdd<-(2^0.25*gamma(0.75)/sqrt(pi))*gamsph(v$h,v$pars)^0.25+e*sig/sqrt(diag(Vare))
+  
+  r1<-v$loores[row(v$loores)<col(v$loores)]
+  tloores<-t(v$loores)
+  r2<-tloores[row(tloores)<col(tloores)]
+  resi<-v$loores-v$loores
+  resi[row(resi)<col(resi)]<-v$res
+  resi<-resi+t(resi)
 
-r1<-v$loores[row(v$loores)<col(v$loores)]
-tloores<-t(v$loores)
-r2<-tloores[row(tloores)<col(tloores)]
-resi<-v$loores-v$loores
-resi[row(resi)<col(resi)]<-v$res
-resi<-resi+t(resi)
-
-n0<-length(v$lofa)
-xn<-v$data[,c(2,1)]
-xy<-matrix(0,n,4)
-te<-crossprod(matrix(1,1,n0),t(xn[,1]))
-xy[,1]<-te[row(te)<col(te)]
-te<-crossprod(t(xn[,1]),matrix(1,1,n0))
-xy[,2]<-te[row(te)<col(te)]
-te<-crossprod(matrix(1,1,n0),t(xn[,2]))
-xy[,3]<-te[row(te)<col(te)]
-te<-crossprod(t(xn[,2]),matrix(1,1,n0))
-xy[,4]<-te[row(te)<col(te)]
-
-if(!missing(xyi)){
+  n0<-length(v$lofa)
+  xn<-v$data[,c(2,1)]
+  xy<-matrix(0,n,4)
+  te<-crossprod(matrix(1,1,n0),t(xn[,1]))
+  xy[,1]<-te[row(te)<col(te)]
+  te<-crossprod(t(xn[,1]),matrix(1,1,n0))
+  xy[,2]<-te[row(te)<col(te)]
+  te<-crossprod(matrix(1,1,n0),t(xn[,2]))
+  xy[,3]<-te[row(te)<col(te)]
+  te<-crossprod(t(xn[,2]),matrix(1,1,n0))
+  xy[,4]<-te[row(te)<col(te)]
+  
+  if(!missing(xyi)){
    ix<-ceiling(sqrt((2*(xyi)+0.25))-0.5)+1
    iy<-(xyi)-ceiling(sqrt((2*(xyi)+0.25))-0.5)/2*(ceiling(sqrt((2*(xyi)+0.25))-0.5)-1)
    nl<-n
    #*(n-1)/2
    ind1<-ceiling(sqrt((2*(1:nl)+0.25))-0.5)+1
    ind2<-(1:nl)-ceiling(sqrt((2*(1:nl)+0.25))-0.5)/2*(ceiling(sqrt((2*(1:nl)+0.25))-0.5)-1)
-}
+  }
 
-#graphsheet(win.width=0.8,win.height=1,win.left=0,win.top=0,Name="Interactive Variogram Plot")
-#windows(width = 8, height = 5.5,rescale="R")
-#windows()
-par(mfrow=c(2,2),bg="white")
-par(mfg=c(2,1,2,2))
-#graphsheet(win.width=0.5,win.height=0.5,win.left=0,win.top=0.5,Name="3) LOO")
-plot(matrix(cbind(v$res,v$res),n*2,1),matrix(cbind(r1,r2),n*2,1),pch=1,xlab="",ylab="Leave one out residuals",lwd=1)
-lines(c(min(v$res),max(v$res)),c(min(v$res),max(v$res)),col=8)
-segments(v$res,r1,v$res,r2)
-title("Leave One Out Residuals")
-if(!missing(xyi)){
-print("xyi")
-print(xyi)
+  paro<-par(no.readonly=TRUE)
+  par(mfrow=c(2,2), mar = c(3,2,2,1)+.1, bg="white")
+
+#EJP: moved (1,1) to be first plotted:
+  #graphsheet(win.width=0.5,win.height=0.5,win.left=0,win.top=0,Name="1) MAP")
+  # plot map view as left plot in first row
+  dg<-1:length(v$lofa)
+  for(i in 1:length(v$lofa)){
+    dg[i]<-sum((0.822*(gamsph(v$h,c(v$tha[i,1],v$tha[i,2],v$tha[i,3]))^0.25-gamsph(v$h,v$pars)^0.25))^2)
+  }
+  #if(! exists("zmv")) zmv<-0
+  #if(zmv==0) plot(xn[,2],xn[,1],xlim=c(-0.6,1.1),ylim=c(-0.28, 0.28))
+  #if(zmv==0) plot(xn[,2],xn[,1],xlim=c(-1.1,1.1),ylim=c(-0.28, 0.28),lwd=3)
+  if (zmv==0 && !is.null(region))
+    plot(xn[,2],xn[,1],xlim=c(min(region[,1]), max(region[,1])),
+		ylim=c(min(region[,2]), max(region[,2])),lwd=3, 
+		asp = 1, xlab = "", ylab = "")
+  else
+    plot(xn[,2],xn[,1],xlim=c(min(xn[,2]), max(xn[,2])),
+		ylim=c(min(xn[,1]), max(xn[,1])),lwd=3, 
+		asp = 1, xlab = "", ylab = "")
+  #lolo<-lof[1,1]-lofa
+  z<-xn[,1]
+  if(zmv>0){
+    z<-switch(zmv,v$data[,3],v$cda,v$lofa,dg)
+    inc<-0.25
+    rmin<-0.03
+    epsi<-(max(z)-min(z))/(inc/rmin-1)
+  #  symbols(xn[,2],xn[,1],circles=z-min(z)+epsi,inches=inc,xlim=c(-0.63,1.14),ylim=c(-0.3, 0.28))
+    if(zmv>0 && !is.null(region))
+      symbols(xn[,2],xn[,1],circles=z-min(z)+epsi,inches=inc,
+	  	xlim=c(min(region[,1]), max(region[,1])),
+		ylim=c(min(region[,2]), max(region[,2])),lwd=3)
+    else
+      symbols(xn[,2],xn[,1],circles=z-min(z)+epsi,inches=inc,
+	  	xlim=c(min(xn[,2]), max(xn[,2])),
+		ylim=c(min(xn[,1]), max(xn[,1])),lwd=3)
+    #  symbols(xn[,2],xn[,1],circles=z-min(z)+epsi,inches=inc,lwd=3)
+  }
+  if(!is.null(region)) polygon(region[,1],region[,2],density=0,col=2)
+    title(paste("Map View",switch(zmv+1,'','(y)',"(Cook's Distance)",'(Mahalanobis Distance)',"(Cook's Distance)")))
+  #gsdmpv<-dev.cur()
+
+  if(!missing(xyi)){
+   segments(xy[xyi,3],xy[xyi,1],xy[xyi,4],xy[xyi,2],pch=16,col=3,lwd=3)
+   points(xy[xyi,3],xy[xyi,1],pch=16,col=6)
+   points(xy[xyi,4],xy[xyi,2],pch=16,col=8)
+#   identify(xn[,2],xn[,1],plot=T,pts=cbind(xn[ix,2],xn[ix,1]))
+   text(xn[ix,2],xn[ix,1]-(max(z)-min(z))/10,paste(ix))
+#   identify(xn[,2],xn[,1],plot=T,pts=cbind(xn[iy,2],xn[iy,1]))
+   text(xn[iy,2],xn[iy,1]-(max(z)-min(z))/10,paste(iy))
+  }
+  assign("p11", par(no.readonly=TRUE), envir = .varDiagOptions)
+  # EJP-end
+
+  #graphsheet(win.width=0.8,win.height=1,win.left=0,win.top=0,Name="Interactive Variogram Plot")
+  #windows(width = 8, height = 5.5,rescale="R")
+  #windows()
+  par(mfg=c(2,1))
+  #graphsheet(win.width=0.5,win.height=0.5,win.left=0,win.top=0.5,Name="3) LOO")
+  plot(matrix(cbind(v$res,v$res),n*2,1), matrix(cbind(r1,r2),n*2,1),
+  	pch=1, xlab="", ylab="", lwd=1)
+  lines(c(min(v$res),max(v$res)),c(min(v$res),max(v$res)),col=8)
+  segments(v$res,r1,v$res,r2)
+  title("Leave One Out Residuals")
+  if(!missing(xyi)){
+    print("xyi")
+    print(xyi)
    points(v$res[xyi],r1[xyi],pch=18,col=3)
    points(v$res[xyi],r2[xyi],pch=18,col=5)
    points(t(resi[ix,-ix]),t(v$loores[ix,-ix]),pch=16,col=6)
    points(t(resi[iy,-iy]),t(v$loores[iy,-iy]),pch=16,col=8)
    segments(v$res[xyi],r1[xyi],v$res[xyi],r2[xyi],col=3,lwd=5)
-}
-p21<<-par(no.readonly=TRUE)
+  }
+  assign("p21", par(no.readonly=TRUE), envir = .varDiagOptions)
 
-cv1<-cv
-i<-1:n
-di<-dim(v$cg)[1]
-if(!missing(xyi)){
+  cv1<-cv
+  i<-1:n
+  di<-dim(v$cg)[1]
+  if(!missing(xyi)){
 #   di<-dim(v$cg)[1]
 #   pm<-diag(di)
 #   pm[xyi,]<-diag(di)[di,]
@@ -253,92 +319,39 @@ if(!missing(xyi)){
    cg1<-v$cg[i,i]
    infm<-solve(cg1);
    cv1<-chol((infm+t(infm))/2);
-}
-par(mfg=c(2,2,2,2))
-#graphsheet(win.width=0.5,win.height=0.5,win.left=0.5,win.top=0.5,Name="4) DCR")
-x<-((2^0.25*gamma(0.75)/sqrt(pi))*gamsph(v$h,v$pars)^0.25)[i]
-y<-v$res[i]
-cv1<-cv1/cv1[di,di]
-plot(cv1%*%x,cv1%*%y,xlab="",ylab="",lwd=1)
-if(!missing(xyi)){
+  }
+  par(mfg=c(2,2))
+  #graphsheet(win.width=0.5,win.height=0.5,win.left=0.5,win.top=0.5,Name="4) DCR")
+  x<-((2^0.25*gamma(0.75)/sqrt(pi))*gamsph(v$h,v$pars)^0.25)[i]
+  y<-v$res[i]
+  cv1<-cv1/cv1[di,di]
+  plot(cv1%*%x,cv1%*%y,xlab="",ylab="",lwd=1)
+  if(!missing(xyi))
     points(x[n],y[n],pch=16,col=3)
-}
-#sm<-lowess(cv1%*%x,cv1%*%y)
-#lines(sm$x,sm$y,lwd=3)
-glu<-min(cv1%*%x)
-glo<-max(cv1%*%x)
-lines(c(glu,glo),c(0,0))
-title("Decorrelated Residuals")
-p22<<-par(no.readonly=TRUE)
+  #sm<-lowess(cv1%*%x,cv1%*%y)
+  #lines(sm$x,sm$y,lwd=3)
+  glu<-min(cv1%*%x)
+  glo<-max(cv1%*%x)
+  lines(c(glu,glo),c(0,0))
+  title("Decorrelated Residuals")
+  assign("p22", par(no.readonly=TRUE), envir = .varDiagOptions)
 
-xv<-seq(0.0001,max(v$h),0.01)
-par(mfg=c(1,2,2,2))
-#graphsheet(win.width=0.5,win.height=0.5,win.left=0.5,win.top=0,Name="2) SVC")
-plot(v$h,gdd,xlab="",ylab="",lwd=1)
-lines(xv,(2^0.25*gamma(0.75)/sqrt(pi))*gamsph(xv,v$pars)^0.25,lwd=3)
-title("Studentized Square Root Cloud")
-if(!missing(xyi)){
+  xv<-seq(0.0001,max(v$h),0.01)
+  par(mfg=c(1,2))
+  #graphsheet(win.width=0.5,win.height=0.5,win.left=0.5,win.top=0,Name="2) SVC")
+  plot(v$h,gdd,xlab="",ylab="",lwd=1)
+  lines(xv,(2^0.25*gamma(0.75)/sqrt(pi))*gamsph(xv,v$pars)^0.25,lwd=3)
+  title("Studentized Square Root Cloud")
+  if(!missing(xyi)){
    points(v$h[ind1==ix | ind2 == ix],gdd[ind1==ix | ind2 == ix],pch=16,col=6)
    points(v$h[ind1==iy | ind2 == iy],gdd[ind1==iy | ind2 == iy],pch=16,col=8)
    points(v$h[xyi],gdd[xyi],pch=16,col=3)
-}
+  }
 
-p12<<-par(no.readonly=TRUE)
+  assign("p12", par(no.readonly=TRUE), envir = .varDiagOptions)
 
-#graphsheet(win.width=0.5,win.height=0.5,win.left=0,win.top=0,Name="1) MAP")
-paro<-par(no.readonly=TRUE)
-# plot map view as left plot in first row
-par(mfg=c(1,1,2,2))
-di0<-par("din")-0.5
-wth<-di0[1]/di0[2]
-ar<-(max(xn[,2])-min(xn[,2]))/(max(xn[,1])-min(xn[,1]));
-if(!missing(region)) ar<-(max(region[,1])-min(region[,1]))/
-    (max(region[,2])-min(region[,2]));
-if(ar>wth) di0[2]<-di0[1]/ar else di0[1]<-di[2]*ar;
-par(mai=c(0.5,0.5,1,0))
-#di0[2]<-di0[1]*0.58/2.32
-par(pin=di0/2)
-dg<-1:length(v$lofa)
-for(i in 1:length(v$lofa)){
-    dg[i]<-sum((0.822*(gamsph(v$h,c(v$tha[i,1],v$tha[i,2],v$tha[i,3]))^0.25-gamsph(v$h,v$pars)^0.25))^2)
-}
-#if(! exists("zmv")) zmv<-0
-#if(zmv==0) plot(xn[,2],xn[,1],xlim=c(-0.6,1.1),ylim=c(-0.28, 0.28))
-#if(zmv==0) plot(xn[,2],xn[,1],xlim=c(-1.1,1.1),ylim=c(-0.28, 0.28),lwd=3)
-if(zmv==0) if(!missing(region))
-    plot(xn[,2],xn[,1],xlim=c(min(region[,1]), max(region[,1])),ylim=c(min(region[,2]), max(region[,2])),lwd=3)
-else
-    plot(xn[,2],xn[,1],xlim=c(min(xn[,2]), max(xn[,2])),ylim=c(min(xn[,1]), max(xn[,1])),lwd=3)
-#lolo<-lof[1,1]-lofa
-z<-xn[,1]
-if(zmv>0){
-  z<-switch(zmv,v$data[,3],v$cda,v$lofa,dg)
-  inc<-0.25
-  rmin<-0.03
-  epsi<-(max(z)-min(z))/(inc/rmin-1)
-#  symbols(xn[,2],xn[,1],circles=z-min(z)+epsi,inches=inc,xlim=c(-0.63,1.14),ylim=c(-0.3, 0.28))
-if(zmv>0) if(!missing(region))
-    symbols(xn[,2],xn[,1],circles=z-min(z)+epsi,inches=inc,xlim=c(min(region[,1]), max(region[,1])),ylim=c(min(region[,2]), max(region[,2])),lwd=3)
-else
-    symbols(xn[,2],xn[,1],circles=z-min(z)+epsi,inches=inc,xlim=c(min(xn[,2]), max(xn[,2])),ylim=c(min(xn[,1]), max(xn[,1])),lwd=3)
-#  symbols(xn[,2],xn[,1],circles=z-min(z)+epsi,inches=inc,lwd=3)
-}
-if(!missing(region)) polygon(region[,1],region[,2],den=0,col=2)
-title(paste("Map View",switch(zmv+1,'','(y)',"(Cook's Distance)",'(Mahalanobis Distance)',"(Cook's Distance)")))
-#gsdmpv<-dev.cur()
-
-if(!missing(xyi)){
-   segments(xy[xyi,3],xy[xyi,1],xy[xyi,4],xy[xyi,2],pch=16,col=3,lwd=3)
-   points(xy[xyi,3],xy[xyi,1],pch=16,col=6)
-   points(xy[xyi,4],xy[xyi,2],pch=16,col=8)
-#   identify(xn[,2],xn[,1],plot=T,pts=cbind(xn[ix,2],xn[ix,1]))
-   text(xn[ix,2],xn[ix,1]-(max(z)-min(z))/10,paste(ix))
-#   identify(xn[,2],xn[,1],plot=T,pts=cbind(xn[iy,2],xn[iy,1]))
-   text(xn[iy,2],xn[iy,1]-(max(z)-min(z))/10,paste(iy))
-}
-p11<<-par(no.readonly=TRUE)
-par(paro)
-n
+  par(paro)
+  n
 }
 
 CookRLF.varobj<-function(v){
@@ -365,7 +378,9 @@ QQVarcloud.varobj<-function(v){
 n<-length(v$h)
 infm<-solve(v$cg);
 cv<-chol((infm+t(infm))/2);
-plot(qnorm(seq(from=1/(2*n),length=n,by=1/n)),sort(cv%*%v$y),xlab="Quantile der Standardnormalverteilung",ylab="Geordnete dekorrelierte Residuen")
+plot(qnorm(seq(from=1/(2*n),length=n,by=1/n)),sort(cv%*%v$y),
+	xlab="quantile of standard normal distribution",
+	ylab="orderd decorrelated residual")
 lines(c(-3,3),c(-3,3),col=8,lwd=3)
 apply(t(apply(apply(matrix(rnorm(n*100),ncol=100),2,sort),1,quantile,probs=c(0.05/n,1-0.05/n))),2,lines,x=qnorm(seq(from=1/(2*n),length=n,by=1/n)))
 }
@@ -376,12 +391,14 @@ QQDecorr.varobj<-function(v){
 n<-length(v$h)
 infm<-solve(v$cg);
 cv<-chol((infm+t(infm))/2);
-plot(qchisq(seq(from=1/(2*n),length=n,by=1/n),1),sort(v$y/gamsph(v$h,v$pars)),xlab="Quantile der Chi-Quadrat Verteilung",ylab="Geordnete Werte von [Z(s)-Z(s')]^2/2g(s-s')")
+plot(qchisq(seq(from=1/(2*n),length=n,by=1/n),1),sort(v$y/gamsph(v$h,v$pars)),
+	xlab="quantile of Chi-square distribution",
+	ylab="ordered value of [Z(s)-Z(s')]^2/(2g(s-s'))")
 apply(t(apply(apply(matrix(rchisq(n*100,1),ncol=100),2,sort),1,quantile,probs=c(0.05/n,1-0.05/n))),2,lines,x=qchisq(seq(from=1/(2*n),length=n,by=1/n),1))
 lines(c(0,8),c(0,8),col=8,lwd=3)
 }
 
-interact.varobj<-function(v,region=0,g="s",pchi=0.05,zmv=0){
+interact.varobj<-function(v,region=NULL,g="s",pchi=0.05,zmv=0){
 #Identifikation in studentisierter VC
 
 palette(c("black","cyan","magenta","green3","yellow","blue","white","red"))
@@ -416,38 +433,35 @@ te<-crossprod(t(xn[,2]),matrix(1,1,n0))
 xy[,4]<-te[row(te)<col(te)]
 
 if(g=="l"){
-   par(mfrow=c(2,2))
-   par(mfg=c(2,1,2,2))
-   par(p21)
+   par(mfrow=c(2,2), mfg=c(2,1))
+   par(get("p21", envir = .varDiagOptions))
    par(fig=c(0,0.5,0,0.5))
    xyi<-identify(matrix(cbind(v$res,v$res),n*2,1),matrix(cbind(r1,r2),n*2,1),plot=FALSE,n=1)
    if(xyi>n) xyi<-xyi-n
 }
 
 if(g=="m"){
-   par(mfrow=c(2,2))
-   par(mfg=c(1,1,2,2))
-   par(p11)
+   par(mfrow=c(2,2), mfg=c(1,1))
+   par(get("p11", envir = .varDiagOptions))
    par(fig=c(0,0.5,0.5,1))
    ix0<-identify(xn[,2],xn[,1],plot=TRUE,n=1)
    points(xn[ix0,2],xn[ix0,1],pch=16,col=6)
 
       ind1<-ceiling(sqrt((2*(1:n)+0.25))-0.5)+1
       ind2<-(1:n)-ceiling(sqrt((2*(1:n)+0.25))-0.5)/2*(ceiling(sqrt((2*(1:n)+0.25))-0.5)-1)
-      par(p12)
-      par(mfrow=c(2,2),mfg=c(1,2,2,2),fig=c(0.5,1,0.5,1))
+      par(get("p12", envir = .varDiagOptions))
+      par(mfrow=c(2,2),mfg=c(1,2),fig=c(0.5,1,0.5,1))
 #      par(mfg=c(1,2,2,2))
 #      par(fig=c(0.5,1,0.5,1))
       points(v$h[ind1==ix0 | ind2 == ix0],gdd[ind1==ix0 | ind2 == ix0],pch=16,col=6)
-      par(mfrow=c(2,2))
-      par(mfg=c(2,1,2,2))
-      par(p21)
+      par(mfrow=c(2,2), mfg=c(2,1))
+      #par(p21)
+      par(get("p21", envir = .varDiagOptions))
       par(fig=c(0,0.5,0,0.5))
       points(t(resi[ix0,-ix0]),t(v$loores[ix0,-ix0]),pch=16,col=6)
 
-      par(mfrow=c(2,2))
-   par(mfg=c(1,1,2,2))
-   par(p11)
+      par(mfrow=c(2,2), mfg=c(1,1))
+   par(get("p11", envir = .varDiagOptions))
    par(fig=c(0,0.5,0.5,1))
    iy0<-identify(xn[,2],xn[,1],plot=FALSE,n=1)
    if(length(iy0)>0){
@@ -466,31 +480,31 @@ if(g=="m"){
 #      identify(xn[,2],xn[,1],plot=T,pts=cbind(xn[ix0,2],xn[ix0,1]))
       ind1<-ceiling(sqrt((2*(1:n)+0.25))-0.5)+1
       ind2<-(1:n)-ceiling(sqrt((2*(1:n)+0.25))-0.5)/2*(ceiling(sqrt((2*(1:n)+0.25))-0.5)-1)
-      par(mfrow=c(2,2))
-      par(mfg=c(1,2,2,2))
-      par(p12)
+      par(mfrow=c(2,2), mfg=c(1,2))
+      #par(p12)
+      par(get("p12", envir = .varDiagOptions))
       par(fig=c(0.5,1,0.5,1))
       points(v$h[ind1==ix0 | ind2 == ix0],gdd[ind1==ix0 | ind2 == ix0],pch=16,col=6)
-      par(mfrow=c(2,2))
-      par(mfg=c(2,1,2,2))
-      par(p21)
+      par(mfrow=c(2,2), mfg=c(2,1))
+      #par(p21)
+      par(get("p21", envir = .varDiagOptions))
       par(fig=c(0,0.5,0,0.5))
       points(t(resi[ix0,-ix0]),t(v$loores[ix0,-ix0]),pch=16,col=6)
       }
 }
 
 if(g=="s"){
-   par(mfrow=c(2,2))
-   par(mfg=c(1,2,2,2))
-   par(p12)
+   par(mfrow=c(2,2), mfg=c(1,2))
+   #par(p12)
+   par(get("p12", envir = .varDiagOptions))
    par(fig=c(0.5,1,0.5,1))
    xyi<-identify(v$h,gdd,plot=FALSE,n=1)
 }
 
 if(g=="t"){
-   par(mfrow=c(2,2))
-   par(mfg=c(1,2,2,2))
-   par(p12)
+   par(mfrow=c(2,2), mfg=c(1,2))
+   #par(p12)
+   par(get("p12", envir = .varDiagOptions))
    par(fig=c(0.5,1,0.5,1))
    p<-locator(n=500,type="l",col=4)
    m<-length(p$x)
@@ -501,14 +515,15 @@ if(g=="t"){
    i0<-(s%%2)|(gt%%2)
    dev.off()
    PlotDiag.varobj(v,region)
-   par(mfg=c(1,2,2,2))
-   par(p12)
+   par(mfg=c(1,2))
+   #par(p12)
+   par(get("p12", envir = .varDiagOptions))
    par(fig=c(0.5,1,0.5,1))
    points(v$h[i0],gdd[i0],pch=16,col=3)
-   polygon(p,den=0,col=4)
+   polygon(p,density=0,col=4)
 
-   par(mfg=c(1,1,2,2))
-   par(p11)
+   par(mfg=c(1,1))
+   par(get("p11", envir = .varDiagOptions))
    par(fig=c(0,0.5,0.5,1))
    segments(xy[i0,3],xy[i0,1],xy[i0,4],xy[i0,2],pch=16,col=3,lwd=3)
 
@@ -516,17 +531,18 @@ if(g=="t"){
 }
 
 if(g=="x"){
-   par(mfrow=c(2,2))
-   par(mfg=c(1,2,2,2))
-   par(p12)
+   par(mfrow=c(2,2), mfg=c(1,2))
+   #par(p12)
+   par(get("p12", envir = .varDiagOptions))
    par(fig=c(0.5,1,0.5,1))
 
    i0<-(gdd-(2^0.25*gamma(0.75)/sqrt(pi))*gamsph(v$h,v$pars)^0.25)/sig>qnorm(1-pchi/2)
    i0a<-(-gdd+(2^0.25*gamma(0.75)/sqrt(pi))*gamsph(v$h,v$pars)^0.25)/sig>qnorm(1-pchi/2)
    dev.off()
    PlotDiag.varobj(v,region,zmv=zmv)
-   par(mfg=c(1,2,2,2))
-   par(p12)
+   par(mfg=c(1,2))
+   #par(p12)
+   par(get("p12", envir = .varDiagOptions))
    par(fig=c(0.5,1,0.5,1))
    points(v$h[i0],gdd[i0],pch=16,col=3)
    points(v$h[i0a],gdd[i0a],pch=16,col=4)
@@ -536,10 +552,10 @@ if(g=="x"){
 #   lines(xv,gamsph(xv,v$pars)*qchisq(1-pchi,1),lty=4,lwd=2)
    lines(xv,(2^0.25*gamma(0.75)/sqrt(pi))*gamsph(xv,v$pars)^0.25+sig*qnorm(1-pchi/2),lty=4,lwd=2)
    lines(xv,(2^0.25*gamma(0.75)/sqrt(pi))*gamsph(xv,v$pars)^0.25-sig*qnorm(1-pchi/2),lty=4,lwd=2)
-   par(p11)
+   par(get("p11", envir = .varDiagOptions))
 
-   par(mfg=c(1,1,2,2))
-   par(p11)
+   par(mfg=c(1,1))
+   par(get("p11", envir = .varDiagOptions))
 print(xy[i0,])
    par(fig=c(0,0.5,0.5,1))
    segments(xy[i0,3],xy[i0,1],xy[i0,4],xy[i0,2],pch=16,col=3,lwd=2)
@@ -549,9 +565,8 @@ print(xy[i0,])
 }
 
 if(g=="n"){
-   par(mfrow=c(2,2))
-   par(mfg=c(1,1,2,2))
-   par(p11)
+   par(mfrow=c(2,2), mfg=c(1,1))
+   par(get("p11", envir = .varDiagOptions))
    par(fig=c(0,0.5,0.5,1))
    p<-locator(n=500,type="l",pch=16,col=4)
    m<-length(p$x)
@@ -566,17 +581,18 @@ if(g=="n"){
    i00<-match(ind1,(1:n0)[i0],nomatch=FALSE)&match(ind2,(1:n0)[i0],nomatch=FALSE)
    dev.off()
    PlotDiag.varobj(v,region)
-   par(mfg=c(1,2,2,2))
-   par(p12)
+   par(mfg=c(1,2))
+   #par(p12)
+   par(get("p12", envir = .varDiagOptions))
    par(fig=c(0.5,1,0.5,1))
    points(v$h[i00],gdd[i00],pch=16,col=3)
 
-   par(mfg=c(1,1,2,2))
-   par(p11)
+   par(mfg=c(1,1))
+   par(get("p11", envir = .varDiagOptions))
    par(fig=c(0,0.5,0.5,1))
-   polygon(p,den=0,col=4)
+   polygon(p,density=0,col=4)
    segments(xy[i00,3],xy[i00,1],xy[i00,4],xy[i00,2],pch=16,col=3,lwd=3)
-
+   xyi = 0
 }
 
 #print(xyi)
